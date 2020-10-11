cf133e7a6aa5792f897cf4c98531c63da3a60564
Mike Bronner <mike@genealabs.com>
2020-10-11 17:37:03 +0000

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phpmd.phar ${INPUT_TARGET_DIRECTORY} ${INPUT_STANDARD}\
+php /usr/local/bin/phpmd.phar ${INPUT_TARGET_DIRECTORY} xml ${INPUT_STANDARD}\
     | reviewdog -name=PHPMD -f=checkstyle -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
