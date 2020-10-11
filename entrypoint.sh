98986450d35852bb83050e4c4642a303a371c97f
Mike Bronner <mike@genealabs.com>
2020-10-11 19:07:21 +0000

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-php /usr/local/bin/phpmd.phar ${INPUT_TARGET_DIRECTORY} text ${INPUT_STANDARD}\
-    | sed -r "s/([0-9]+)\s+/\1:/gI"
+php /usr/local/bin/phpmd.phar ${INPUT_TARGET_DIRECTORY} text ${INPUT_STANDARD} \
+    | sed -r "s/([0-9]+)\s+/\1:/gI" \
     | reviewdog -name=PHPMD -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
