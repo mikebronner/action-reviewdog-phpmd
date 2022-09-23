545e7f31ffdcfe8076209a08d0bce8b84fea2616
Christian Vette <christian.vette@avency.de>
2022-09-23 09:28:00 +0200

    feat: allow to add a baseline file

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,15 @@ fi
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 excludeOption=""
-if [[ "$INPUT_EXCLUDE" != "" ]]; then
+if [ "$INPUT_EXCLUDE" != "" ]; then
     excludeOption=" --exclude $INPUT_EXCLUDE"
 fi;
 
-php /usr/local/bin/phpmd.phar ${INPUT_TARGET_DIRECTORY}${excludeOption} text ${INPUT_STANDARD} \
+baselineFileOption=""
+if [ "$INPUT_BASELINE_FILE" != "" ]; then
+    baselineFileOption=" --baseline-file $INPUT_BASELINE_FILE"
+fi;
+
+php /usr/local/bin/phpmd.phar ${INPUT_TARGET_DIRECTORY}${excludeOption}${baselineFileOption} text ${INPUT_STANDARD} \
     | sed -r "s/([0-9]+)\s+/\1:/gI" \
     | reviewdog -name="${INPUT_NAME}" -f=phpstan -reporter=${INPUT_REPORTER} -level=${INPUT_LEVEL} -diff='git diff'
